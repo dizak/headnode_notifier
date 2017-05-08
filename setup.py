ecee0dd2e0abc74f4d09e8ea61434b1e6eed822a
dizak <dariusz.izak@ibb.waw.pl>
2017-05-08 15:41:34 +0200

    ENH: Setuptools instead of distutils. ENH: Scripts args for installing as executable

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from distutils.core import setup
-from headnode_notifier import __version__ as version
+from setuptools import setup
+from headnode_notifier import __version__ as VERSION
+from headnode_notifier import __author__ as AUTHOR
+
 
 setup(name="headnode_notifier",
-      version=version,
+      version=VERSION,
+      description="Simple script for sending emails",
+      author=AUTHOR,
+      author_email="dariusz.izak@ibb.waw.pl",
+      url="https://github.com/dizak/headnode_notifier",
+      license="MIT",
+      py_modules=["headnode_notifier"],
       scripts=["headnode_notifier.py"])
