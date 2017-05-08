e4f373db189fc857b8e3b293ae520db002823385
dizak <dariusz.izak@ibb.waw.pl>
2017-05-08 14:58:50 +0200

    UPD

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
+from headnode_notifier import __version__ as version
 
 setup(name="headnode_notifier",
-      version="1.1",
-      py_modules=["headnode_notifier"])
+      version=version,
+      scripts=["headnode_notifier.py"])
